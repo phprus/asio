79ebb512d9702ff7948e06da989970643dad0719
Владислав Щапов <phprus@gmail.com>
2021-10-01 01:55:30 +0500

    UPSTREAM: Enable ASIO_HAS_STD_INVOKE_RESULT with libc++13
    Upstream: https://github.com/chriskohlhoff/asio/pull/910

--- asio/include/asio/detail/config.hpp
+++ asio/include/asio/detail/config.hpp
@@ -493,6 +493,13 @@ with the -std=c++11 or -std=gnu++11 compiler options.
 #    define ASIO_HAS_STD_INVOKE_RESULT 1
 #   endif // (_MSC_VER >= 1911 && _MSVC_LANG >= 201703)
 #  endif // defined(ASIO_MSVC)
+#  if defined(ASIO_HAS_CLANG_LIBCXX)
+#   if (_LIBCPP_VERSION >= 13000)
+#    if (__cplusplus >= 202002)
+#     define ASIO_HAS_STD_INVOKE_RESULT 1
+#    endif // (__cplusplus >= 202002)
+#   endif // (_LIBCPP_VERSION >= 13000)
+#  endif // defined(ASIO_HAS_CLANG_LIBCXX)
 # endif // !defined(ASIO_DISABLE_STD_INVOKE_RESULT)
 #endif // !defined(ASIO_HAS_STD_INVOKE_RESULT)
 
