50740af4c8803f213c35e213a6621a72e465c601
Vladislav Shchapov <vladislav@shchapov.ru>
2022-09-17 21:23:55 +0500

    Switch to С++17: std::thread (ASIO_HAS_STD_THREAD).
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- asio/include/asio/detail/std_thread.hpp
+++ asio/include/asio/detail/std_thread.hpp
@@ -17,8 +17,6 @@
 
 #include "asio/detail/config.hpp"
 
-#if defined(ASIO_HAS_STD_THREAD)
-
 #include <thread>
 #include "asio/detail/noncopyable.hpp"
 
@@ -65,7 +63,5 @@ private:
 } // namespace asio
 
 #include "asio/detail/pop_options.hpp"
-
-#endif // defined(ASIO_HAS_STD_THREAD)
 
 #endif // ASIO_DETAIL_STD_THREAD_HPP
